ab2853d2953aad145ba35cc0c82726a1be26c5ef
idrice24 <idrice.tsafouet@yahoo.com>
2020-07-03 02:01:29 +0100

    Create login.js

---
+++ src/pages/login.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class login extends Component{
+    render(){
+        return(
+        <div>
+        <h1>login Page</h1>
+        </div>
+        )
+    }
+}
+export default login
