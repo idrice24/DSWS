452808a421c1372b5cfe32aeb9e4b030cc465521
idrice24 <idrice.tsafouet@yahoo.com>
2020-07-03 02:00:51 +0100

    Create signup.js

---
+++ src/pages/signup.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class signup extends Component{
+    render(){
+        return(
+        <div>
+        <h1>Signup Page</h1>
+        </div>
+        )
+    }
+}
+export default signup
